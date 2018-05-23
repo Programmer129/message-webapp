80d0650f2b09689899ea072ec0ac529c5011f85f
avto <avtobolashvili@mail.com>
2018-05-23 16:25:42 +0400

    image transfer

--- src/main/java/com/springsecurity/demo/controllers/UserController.java
+++ src/main/java/com/springsecurity/demo/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.springsecurity.demo.controllers;
 
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.springsecurity.demo.configurations.MongoTemplateConfig;
+import com.springsecurity.demo.dto.UserDTO;
 import com.springsecurity.demo.dto.UserLoginDTO;
 import com.springsecurity.demo.entities.User;
 import com.springsecurity.demo.services.UserService;
@@ -9,7 +10,8 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
@@ -35,7 +34,6 @@ public class UserController {
     private final HttpSession httpSession;
 
     private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MongoTemplateConfig.class);
-    private GridFsOperations gridFsOperations = (GridFsOperations) context.getBean("gridFsTemplate");
     private GridFSBucket bucket = (GridFSBucket) context.getBean("getFSBucket");
 
     @Autowired
@@ -71,21 +69,25 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping(path = "/get", headers = "content-type=multipart/*")
-    public MultipartFile getUserProfile() {
-        File file = new File("/home/levani/IdeaProjects/demo/src/main/resources/profile.png");
+    @GetMapping(path = "/get-image", headers = "content-type=multipart/*")
+    public Resource getUserProfile() {
+        File file = new File("/home/levani/IdeaProjects/demo/src/main/resources/profile" + httpSession.getAttribute("id")+".png");
+        Resource resource = null;
         try {
             bucket.downloadToStream("profile" + httpSession.getAttribute("id")+".png", new FileOutputStream(file));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        FileItem fileItem = new DiskFileItem("profile.png","multipart/*", false, "profile.png",(int)file.length(), file);
-        try {
+            FileItem fileItem = new DiskFileItem("profile.png","multipart/*", false, "profile.png",(int)file.length(), file);
             fileItem.getOutputStream();
+            resource = new UrlResource(file.toURI());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new CommonsMultipartFile(fileItem);
+        return resource;
+    }
+
+    @GetMapping(path = "/get-name")
+    public UserDTO getUserName() {
+        UserDTO dto = new UserDTO();
+        dto.setUserName(httpSession.getAttribute("id").toString());
+        return dto;
     }
 }
---
+++ src/main/java/com/springsecurity/demo/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.springsecurity.demo.dto;
+
+public class UserDTO {
+
+    private String userName;
+
+    public UserDTO() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
