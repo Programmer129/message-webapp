74fef5b35a83e208a9f3b72734d272a7fbfebdd4
avto <avtobolashvili@mail.com>
2018-05-31 00:35:36 +0400

    Add documentation

--- maven.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-source /etc/environmet
-cd demo
-maven clean install -DskipTests=true
--- src/main/java/com/springsecurity/demo/configurations/HttpSessionListenerConf.java
+++ src/main/java/com/springsecurity/demo/configurations/HttpSessionListenerConf.java
@@ -9,6 +9,9 @@ import javax.persistence.Query;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Component
 @Configuration
@@ -24,6 +27,11 @@ public class HttpSessionListenerConf implements HttpSessionListener {
     @Override
     @Transactional
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        try {
+            Files.delete(Paths.get("/home/levani/IdeaProjects/demo/src/main/resources/profile" + httpSessionEvent.getSession().getAttribute("id")+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Query query = manager.createNativeQuery("update user set is_active=0 where user_name=:name");
         query.setParameter("name", httpSessionEvent.getSession().getAttribute("id").toString());
         query.executeUpdate();
